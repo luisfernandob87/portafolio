1366aae4c1fa9237798b4f45109b74b5897b1a8e
luisfernandob87 <luisfernandob87@gmail.com>
2022-08-19 22:14:34 -0600

    update cv

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,8 @@ import moon from "../img/moon.svg";
 import lang from "../img/lang.svg";
 import linkedin from "../img/linkedin.svg";
 import mail from "../img/mail.svg";
+import pdf from "../img/pdf.png";
+import curriculum from "../img/curriculum.pdf";
 
 const Home = () => {
   return (
@@ -14,7 +16,7 @@ const Home = () => {
         <img src={lang} alt="lenguaje" />
       </div>
       <div className="info">
-        <img src={foto} alt="foto" />
+        {/* <img src={foto} alt="foto" /> */}
         <h1>
           Hola, soy <span>L</span>
           <span>u</span>
@@ -29,11 +31,21 @@ const Home = () => {
           <span>e</span>
           <span>s</span>
         </h1>
-        <h3>Desarrollador Web Front End</h3>
+        <h2>Desarrollador Web Front End</h2>
       </div>
       <div className="enlacesHome">
-        <a href="https://www.linkedin.com/in/luis-morales-barillas/" target="_blank"><img src={linkedin} alt="logo_linkedin" /></a>
-        <a href="mailto:luisfernandob87@gmail.com"><img src={mail} alt="logo_mail" /></a>
+        <a
+          href="https://www.linkedin.com/in/luis-morales-barillas/"
+          target="_blank"
+        >
+          <img src={linkedin} alt="logo_linkedin" />
+        </a>
+        <a href="mailto:luisfernandob87@gmail.com">
+          <img src={mail} alt="logo_mail" />
+        </a>
+        <a href={curriculum} target="_blank">
+          <img src={pdf} alt="logo_pdf" style={{ height: "95.99px" }} />
+        </a>
       </div>
     </section>
   );
